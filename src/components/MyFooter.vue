628e0c8023de22b3fb86ca7ec70ed580ca5becfb
sky <793247534@qq.com>
2023-03-17 16:16:02 +0800

    init

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,32 @@
+<template>
+    <footer>
+      <h6>Copyright © 2023 百日梦想家</h6>
+      <h5>挣钱钱，吃大餐，各地游玩耶耶耶！</h5>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name:'MyFooter',
+    }
+</script>
+
+<style scoped>
+  footer{
+    box-sizing: border-box;
+    height: 9.375rem;
+    padding: 3.125rem;
+  }
+  h6{
+    width: 200px;
+    height: 40px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  h5{
+    width: 500px;
+    height: 50px;
+    margin: 0 auto;
+    text-align: center;
+  }
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,68 @@
+<template>
+    <header class="clearfix">
+        <!-- 左侧首页链接 -->
+        <router-link to="/" class="left">
+            <h1>百日梦想岛</h1>
+        </router-link>
+        <!-- 中间跳转链接 -->
+        <div class="center">
+            <router-link to="/article">
+              <h3>文库</h3>
+            </router-link>
+            <router-link to="/social">
+              <h3>社交</h3>
+            </router-link>
+            <router-link to="/entertainment">
+              <h3>休闲</h3>
+            </router-link>
+            <router-link to="/web">
+              <h3>关于</h3>
+            </router-link>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name:'MyHeader',
+    }
+</script>
+
+<style lang='less' scoped>
+  header{
+    position: fixed;
+    top: 0;
+    width: 100%;
+    min-width: 81.25rem;
+    height: 6.25rem;
+    background-color: var(--main-bg-color);
+    z-index: 1;
+  }
+  header .left{
+    float: left;
+    margin: 1.875rem;
+  }
+  header .center{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    display: flex;
+    justify-content: center;
+    a{
+        padding: 2.5rem;
+        letter-spacing: .1875rem;
+        color: #000;
+        h3{
+          display: inline-block;
+          background-color: #fff;
+          padding: .3125rem .9375rem;
+          border-radius: 10px;
+        }
+        h3:hover{
+          background-color: #000;
+          color: #fff;
+        }
+    }
+  }
+</style>
